3c508a49c18a08a3f24eb53de2c7a3e373fb7d4c
testUserHsr <aa.sattarov.hsr@gmail.com>
2025-07-13 12:49:02 +0300

    init

---
+++ src/main/java/ru/tech/telegrambot/TelegramEchoBotApplication.java
@@ -0,0 +1,13 @@
+package ru.tech.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramEchoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramEchoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tech/telegrambot/TelegramEchoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tech.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramEchoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
